3af2f18c954b0f370891028174c60124b82b68f0
Joris Vincent <joris.vincent@pennmedicin.upenn.edu>
2018-01-12 16:56:54 -0500

    Rudimentary logger object
    Generates logmessages identical to logging.logging. Does not Handle them yet. Formatting still has to be extracted.

---
+++ +logging/logger.m
@@ -0,0 +1,142 @@
+classdef logger < handle
+    % Logger class for the logging package
+    %
+    % Syntax:
+    %
+    % Description:
+    %    Logger class for a Pythonic logging MATLAB package.
+    %    Heavily modified version of 'log4m': http://goo.gl/qDUcvZ.
+    %    Original author: Dominique Orban <dominique.orban@gmail.com>
+    %
+    % History:
+    %    01/12/18  jv   forked, started modifications.
+    
+    properties (SetAccess = immutable)
+        name;
+    end
+    
+    properties (SetAccess = protected)
+        levels = containers.Map(...
+            {'ALL','CRITICAL','ERROR','WARNING','INFO','DEBUG','TRACE','NOTSET'},...
+            {Inf,50,40,30,20,10,0,-Inf});
+    end
+    
+    properties (Hidden, SetAccess = protected)
+        level = -Inf; % default level for logger is NOTSET
+    end
+    
+    properties (Dependent)
+        effectiveLevel;
+    end
+    
+    %% Constructor
+    methods
+        function self = logger(name, varargin)
+            % Create a new logger object
+            parser = inputParser();
+            parser.addRequired('name', @ischar);
+            parser.addParameter('level', self.level);
+            parser.parse(name, varargin{:});
+            
+            self.name = parser.Results.name;
+            self.level = parser.Results.level;
+        end
+    end
+    
+    %% Logging methods
+    methods
+        function log(self, level, caller, message)
+            % Log a new message
+            level = self.getLevelNumber(level);
+            if level >= self.effectiveLevel
+                fprintf('%-s %-23s %-8s %s\n',caller,datestr(now,'yyyy-mm-dd HH:MM:SS,FFF'), self.getLevelString(level), message);
+            end
+        end
+        
+        function trace(self, message)
+            % Log a message with level TRACE
+            self.log('TRACE', caller_name, message);
+        end
+        
+        function debug(self, message)
+            % Log a message with level DEBUG
+            [caller_name, ~] = self.findCaller(self);
+            self.log('DEBUG', caller_name, message);
+        end
+        
+        function info(self, message)
+            % Log a message with level INFO
+            [caller_name, ~] = self.findCaller(self);
+            self.log('INFO', caller_name, message);
+        end
+        
+        function warning(self, message)
+            % Log a message with level WARNING
+            [caller_name, ~] = self.findCaller(self);
+            self.log('WARNING', caller_name, message);
+        end
+        
+        function error(self, message)
+            % Log a message with level ERROR
+            [caller_name, ~] = self.findCaller(self);
+            self.log('ERROR', caller_name, message);
+        end
+        
+        function critical(self, message)
+            % Log a message with level CRITICAL
+            [caller_name, ~] = self.findCaller(self);
+            self.log('CRITICAL', caller_name, message);
+        end
+    end
+    
+    %% Setters, getters
+    methods
+        function set.level(self, level)
+            % Set the level of this logger
+            level = self.getLevelNumber(level);
+            self.level = level;
+        end
+        
+        function level = get.effectiveLevel(self)
+            % TODO Get the effective level of this logger
+            %  If this logger does not have a level set, go up to the parent
+            %  loggers until we find one.
+            level = self.level;
+        end
+        
+        function level = getLevelNumber(self, level)
+            % Convert stringliteral level to numeric level code
+            if ischar(level)
+                assert(isKey(self.levels,level),'Level %s not defined for this logger');
+                level = self.levels(level);
+            end
+        end
+        function level = getLevelString(self, level)
+        % Convert numeric level code to stringliteral level
+            if isnumeric(level)
+                levelsInv = containers.Map(values(self.levels),keys(self.levels));
+                assert(isKey(levelsInv,level),'Level %d does not have a label in this logger');
+                level = levelsInv(level);
+            elseif ischar(level)
+                assert(isKey(self.levels,level),'Level %s not defined for this logger');
+            end
+        end
+    end
+    
+    %% Static methods
+    methods(Static)
+        function [name, line] = findCaller(self)
+            % Find out who called the logger
+%             if nargin > 0 && self.ignoreLogging()
+%                 name = [];
+%                 line = [];
+%                 return
+%             end
+            [ST, ~] = dbstack();
+            offset = min(size(ST, 1), 3);
+            name = ST(offset).name;
+            line = ST(offset).line;
+        end
+    end
+    
+end
